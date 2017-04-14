f1c6749c12ea00ce2259143bfe4ddcde5ce7babe
Tony Warwick <tony@eventespresso.com>
2017-04-14 20:19:22 +0100

    Add plugin to remove rows from the EVENTS_PRICES_TABLE and EVENTS_START_END_TABLE tables where event_id = 0;

---
+++ espresso-remove-broken-rows.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Plugin Name: EE3 Remove broken rows
+ * Description: Removes broken rows create by using older version of EE3 with the latest version of WP (activate once and then delete this plugin).
+ * Version: 1.0.0
+ * Author: Tony Warwick
+ */
+
+function ee_remove_broken_rows() {
+
+	global $wpdb;
+
+	$deleted_rows = array();
+	//Remove price rows where event_id == 0
+	$deleted_rows['prices_rows'] = $wpdb->delete( EVENTS_PRICES_TABLE, array( 'event_id' => '0' ), array( '%d' ) );
+	//Remove start/end time rows where event_id == 0
+	$deleted_rows['times_rows'] = $wpdb->delete( EVENTS_START_END_TABLE, array( 'event_id' => '0' ), array( '%d' ) );
+	//Add a notice if rows were effected.
+	if( $deleted_rows['prices_rows'] || $deleted_rows['times_rows'] ) {
+		set_transient( 'ee_remove_broken_rows_success', $deleted_rows, MINUTE_IN_SECONDS );
+	}
+}
+
+add_action( 'admin_notices', 'ee_remove_broken_rows_success_message');
+
+function ee_remove_broken_rows_success_message() {
+
+	$deleted_rows = get_transient( 'ee_remove_broken_rows_success' );
+
+	if( $deleted_rows['prices_rows'] || $deleted_rows['times_rows'] ) {
+	
+		if( $deleted_rows['prices_rows'] && $deleted_rows['times_rows'] ) {
+			$message = 'This plugin removed ' . $deleted_rows['prices_rows'] . ' rows from the ' . EVENTS_PRICES_TABLE . ' table and ' . $deleted_rows['times_rows'] . ' rows from the ' . EVENTS_START_END_TABLE . ' table';
+		} elseif ( $deleted_rows['prices_rows'] ) {
+			$message = 'This plugin removed ' . $deleted_rows['prices_rows'] . ' rows from the ' . EVENTS_PRICES_TABLE . ' table';
+		} elseif ($deleted_rows['times_rows'] ) {
+			$message = 'This plugin removed ' . $deleted_rows['times_rows'] . ' rows from the ' . EVENTS_START_END_TABLE . ' table';
+		} else {
+			$total_deleted = $deleted_rows['prices_rows'] + $deleted_rows['times_rows'];
+		
+			$message = 'This plugin removed a total of ' . $total_deleted . ' rows from the ' . EVENTS_PRICES_TABLE . ' and/or ' . EVENTS_START_END_TABLE . ' tables';
+		}
+
+		?>
+			<div class="notice notice-success is-dismissible">
+				<p><?php echo $message; ?></p>
+			</div>
+		<?php
+	}
+	delete_transient( 'ee_remove_broken_rows_success' );
+}
+register_activation_hook( __FILE__, 'ee_remove_broken_rows' );
